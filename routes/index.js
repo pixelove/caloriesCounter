1e916da9c64112e621007b9ee9a888f4b61bb966
pixelove <pixeloved@gmail.com>
2018-03-26 23:16:28 +0200

    showing data

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const MongoModels = require('../models/mongo');
 function registerRoutes(app) {
   app.use('/public', express.static('public'));
 
-  app.get('/', function (req, res) {
-    MongoModels.Diary.find({}, function (err, r) {
+  app.get('/', function (req,res) {
+    // const now = new Date;
+    // const today = now.getFullYear() + "-" + now.getMonth() + "-" + now.getDate();
+    // console.log(today);
+    const today = "2018-03-26";
+    MongoModels.Diary.find({"day": today}, function (err, r) {
       const data = {
-        diary: JSON.stringify(r)
+        diary: r[0]
       };
       res.render('pages/home', data);
     });
