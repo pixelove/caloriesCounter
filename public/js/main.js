3249f5ef2071e5c6090203152ecb1535397af9b2
pixelove <pixelove@pixeloves-MacBook-Pro.local>
2018-03-27 20:18:40 +0200

    add: product adding, get value

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
   console.log('working');
+
+  function buttonAddProduct() {
+    $('.addProduct').click(
+      function () {
+        // console.log('click');
+        $('#add-lunch-button').hide();
+        $('.add-product-data').show();
+      });
+  };
+
+  function confirmLunchAddition() {
+    $('#add-lunch-confirm').click(
+      function () {
+        let lunchProduct = $('#add-lunch-product').val();
+        let lunchWeight = $('#add-lunch-weight').val();
+
+        $('#add-lunch-button').show();
+        $('.add-product-data').hide();
+        console.log(lunchProduct, lunchWeight);
+      }
+    )
+  }
+
+  confirmLunchAddition();
+  buttonAddProduct();
 });
